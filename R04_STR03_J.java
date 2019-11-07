fadd12f9dc5b8aeca4e8c7e6e4a3289a1b11705a
Otis Diaz <diazotis@gmail.com>
2019-11-07 18:48:13 -0500

    Updated with compliant code and compilable example

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,5 +1,11 @@
 /* Rule 04. Characters and Strings (STR)
  * STR03-J. Do not encode noncharacter data as a string
+ * Non-Compliant Code:
+ * BigInteger x = new BigInteger("530500452766");
+ * byte[] byteArray = x.toByteArray();
+ * String s = new String(byteArray);
+ * byteArray = s.getBytes();
+ * x = new BigInteger(byteArray);
 */
 
 import java.math.BigInteger;
@@ -9,9 +15,11 @@ public class Main
 	public static void main(String[] args)
 	{
 		BigInteger x = new BigInteger("530500452766");
-		byte[] byteArray = x.toByteArray();
-		String s = new String(byteArray);
-		byteArray = s.getBytes();
-		x = new BigInteger(byteArray);
+		String s = x.toString();
+		byte[] byteArray = s.getBytes();
+		String ns = new String(byteArray);
+		x = new BigInteger(ns);
+
+		System.out.println("x: "+x);
 	}
 }
