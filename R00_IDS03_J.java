53b5fb2074778d0ab9a3d9e50ab1bf1def6246bf
Otis Diaz <diazotis@gmail.com>
2019-11-07 18:34:09 -0500

    Updated with compliant code and compilable example

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,5 +1,10 @@
 /* Rule 00. Input Validation and Data Sanitization (IDS)
  * IDS03-J. Do not log unsanitiized user input
+ * Non-Compliant Code:
+ * if (loginSuccessful)
+ * { logger.severe("User login succeeded for: "+username); }
+ * else
+ * { logger.severe("User login failed for: "+username); }
 */
 
 import java.util.Scanner;
@@ -9,10 +14,17 @@ public class Main
 {
         public static void main(String[] args)
         {
-		if (loginSuccessful)
-		{ logger.severe("User login succeeded for: "+username); }
-		else
-		{ logger.severe("User login failed for: "+username); }
+                System.out.println("Enter input: ");
+                Scanner keyboard = new Scanner(System.in);
+                String input = keyboard.nextLine();
+                keyboard.close();
+                if (input.equals("student"))
+                { System.out.println("User login succeeded for: "+sanitizeUser(input)); }
+                else
+                { System.out.println("User login failed for: "+sanitizeUser(input)); }
         }
+
+        public static String sanitizeUser(String username)
+        { return (Pattern.matches("[A-Za-z0-9_]+", username) ? username : "unauthorized user"); }
 }
 
