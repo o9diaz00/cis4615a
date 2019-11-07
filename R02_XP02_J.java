090d24a871a7c9383bb32c5e6d75babfd6e19b2a
Otis Diaz <diazotis@gmail.com>
2019-11-07 18:39:17 -0500

    Updated with compliant code and compilable example

--- R02_XP02_J.java
+++ R02_XP02_J.java
@@ -1,5 +1,9 @@
 /* Rule 02. Expressions (EXP)
  * EXP02-J. Do not use the Object.equals() methods to compare two arrays
+ * Non-Compliant Code:
+ * int[] arr1 = new int[20];
+ * int[] arr2 = new int[20];
+ * SYstem.out.println(arr1.equals(arr2));
 */
 
 import java.util.Arrays;
@@ -8,8 +12,8 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		int[] arr1 = new int[20];
-		int[] arr2 = new int[20];
-		System.out.println(arr1.equals(arr2));
+		int arr1[] = new int[20];
+		int arr2[] = new int[20];
+		System.out.println(Arrays.equals(arr1, arr2));
 	}
 }
