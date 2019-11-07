3555dab60f8f7ba6eb34e5da256b87e2ca44493e
Otis Diaz <diazotis@gmail.com>
2019-11-07 18:31:31 -0500

    Updated with compliant code

--- R00_IDS01_J.java
+++ R00_IDS01_J.java
@@ -1,5 +1,14 @@
 /* Rule 00. Input Validation and Data Sanitization (IDS)
  * IDS01-J. Normalize strings before validating them
+ * Non-COmpliant Code:
+ * String s = "\UFE64" + "script" + "\uFE65";
+ * Pattern pattern = Pattern.compile("[<>]");
+ * Matcher matcher = pattern.matcher(s);
+ * if (matcher.find())
+ * { throw new IllegalStateException(); }
+ * else
+ * { // ... }
+ * s = Normalizer.normalize(s, Form.NFKC);
 */
 
 import java.text.Normalizer;
@@ -12,12 +21,14 @@ public class Main
 	public static void main(String[] args)
 	{
 		String s = "\uFE64" + "script" + "\uFE65";
+		// Normalize
+		s = Normalizer.normalize(s, Form.NFKC);
+		// Validate
 		Pattern pattern = Pattern.compile("[<>]");
 		Matcher matcher = pattern.matcher(s);
 		if (matcher.find())
 		{ throw new IllegalStateException(); }
 		else
-		{ /* ...*/ }
-		s = Normalizer.normalize(s, Form.NFKC);
+		{ System.out.println(s); }
 	}
 }
