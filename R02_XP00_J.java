b01c0441c0556386fdca96be25e663003ba9263b
Otis Diaz <diazotis@gmail.com>
2019-11-07 18:37:31 -0500

    Updated with compliant code and compilable example

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,5 +1,11 @@
 /* Rule 02. Expressions (EXP)
  * XP00-J. Do not ignore values returned by methods
+ * Non-Compliant Code:
+ * public void deleteFile() {
+ * 	File someFile = new File("someFileName.txt");
+ *	// Do something with someFile
+ *	someFile.delete();
+ *	}
 */
 
 import java.io.File;
@@ -9,8 +15,17 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		File someFile = new File("someFileName.txt");
-		// Do something with someFile
-		someFile.delete();
+		File f = new File("newFile.txt");
+		Scanner keyboard = new Scanner(System.in);
+		int input = 0;
+		while (input != 1)
+		{
+			System.out.println("What do you want to do with the file ["+f.getName()+"]?");
+			System.out.println("1)Delete\n*)Nothing");
+			input = keyboard.nextInt();
+		}
+		keyboard.close();
+		if (!f.delete())
+		{ System.out.println("Deletion of file ["+f.getName()+"] failed!"); }
 	}
 }
